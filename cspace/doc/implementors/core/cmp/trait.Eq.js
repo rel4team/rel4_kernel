6a2fc3aeb68ee7e90a910cf1f92c20c7dd54f772
CtrlZ233 <328825619@qq.com>
2023-07-27 08:49:11 +0800

    add cspace doc

---
+++ cspace/doc/implementors/core/cmp/trait.Eq.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"cspace":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"cspace/interface/enum.CapTag.html\" title=\"enum cspace::interface::CapTag\">CapTag</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"cspace/interface/struct.cap_t.html\" title=\"struct cspace::interface::cap_t\">cap_t</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
