bf1f8096d701db17ba3a393c8a8e13aec69e6fde
CtrlZ233 <328825619@qq.com>
2023-10-12 15:18:28 +0800

    add baseline branch

---
+++ build.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+#
+# Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+import subprocess
+import sys
+import argparse
+import time
+import os
+import shutil
+from pygments import highlight
+from pygments.lexers import BashLexer
+from pygments.formatters import TerminalFormatter
+
+build_dir = "./build"
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-b', '--baseline', dest="baseline", action="store_true",
+                        help="baseline switch")
+    parser.add_argument('-c', '--cpu', dest="cpu_nums", type=int,
+                        help="kernel & qemu cpu nums", default=1)
+    args = parser.parse_args()
+    return args
+
+def exec_shell(shell_command):
+    ret_code = os.system(shell_command)
+    return ret_code == 0
+
+def clean_config():
+    shell_command = "cd ../kernel && git checkout master"
+    exec_shell(shell_command)
+
+if __name__ == "__main__":
+    args = parse_args()
+    clean_config()
+    progname = sys.argv[0]
+    
+    if os.path.exists(build_dir):
+        shutil.rmtree(build_dir)
+    os.makedirs(build_dir)
+    if args.baseline == True:
+        shell_command = "cd ../kernel && git checkout baseline"
+        if not exec_shell(shell_command):
+            clean_config()
+            sys.exit(-1)
+    else:
+        if not exec_shell("make run"):
+            clean_config()
+            sys.exit(-1)
+    
+    if args.cpu_nums > 1:
+        clean_config()
+        print("unsupport smp")
+        sys.exit(-1)
+    
+    shell_command = "cd ./build && ../../init-build.sh  -DPLATFORM=spike -DSIMULATION=TRUE && ninja"
+    if not exec_shell(shell_command):
+        clean_config()
+        sys.exit(-1)
+    clean_config()
