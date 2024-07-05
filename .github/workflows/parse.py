d63b6176b1e5946ca8b35df246fefc944c90a721
yufeng <321353225@qq.com>
2024-07-06 03:22:15 +0800

    CI: Add new parse framework

---
+++ .github/workflows/parse.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import os
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: ./parse.py <filename>")
+    sys.exit(1)
+
+# Get the output filename
+file_name = sys.argv[1]
+
+# Read File Content
+content = open(file_name).read()
+
+# Check Content contains values
+def contains(s):
+    return content.find(s) != -1
+
+# Pass ALL tests
+assert contains("All is well in the universe")
+# Kernel not paniced
+assert not contains("rel4_kernel: PANICED")
